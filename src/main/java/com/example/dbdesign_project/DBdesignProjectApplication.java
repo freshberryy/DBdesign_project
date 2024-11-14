94ef3aef137a2df24bd91b105852ba508ef3827c
freshberryy <161305732+freshberryy@users.noreply.github.com>
2024-11-14 19:16:18 +0900

    initial_commit

---
+++ src/main/java/com/example/dbdesign_project/DBdesignProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbdesign_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBdesignProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DBdesignProjectApplication.class, args);
+    }
+
+}
